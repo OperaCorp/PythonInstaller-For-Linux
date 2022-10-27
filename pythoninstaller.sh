5560956126f772aafc773f75a4998dce60b82ff9
Opera <106463731+OperaCorp@users.noreply.github.com>
2022-10-27 11:59:23 +1100

    Add files via upload

---
+++ pythoninstaller.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+clear
+echo "[sudo] Installling python"
+sudo apt install python2
+echo "[sudo] Installing python packages"
+sudo apt install python3
+clear
+echo "Python Packages installed Successfully!"
+echo ""
+echo ""
